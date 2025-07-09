8933d2e7d60568941e9df731148eeb6776dfab86
Shadab Khan <shadab.2005@gmail.com>
2025-07-10 02:06:51 +0530

    chore: add hosted manifest redirect

--- apps/bitrefill-mini-app/next.config.js
+++ apps/bitrefill-mini-app/next.config.js
@@ -7,6 +7,16 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination:
+          'https://api.farcaster.xyz/miniapps/hosted-manifest/0197f0e6-b81f-c44a-e103-45d405619376',
+        permanent: false,
+      },
+    ];
+  },
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
