dc033338c8b338e8a07dc3fe254ce79f85c1b2ef
Shadab Khan <shadab.2005@gmail.com>
2025-07-14 03:23:49 +0530

    fix: lint

--- apps/bitrefill-mini-app/src/app/components/Header.tsx
+++ apps/bitrefill-mini-app/src/app/components/Header.tsx
@@ -1,21 +1,20 @@
 'use client';
-import { NeynarAuthButton, SIWN_variant } from '@neynar/react';
 import '@neynar/react/dist/style.css';
 import Link from 'next/link';
 import { SignInButton } from '@farcaster/auth-kit';
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4 bg-white shadow-md h-10">
+    <header className="flex items-center justify-between p-4 bg-white shadow-md h-14">
       <Link href="/" className="text-xl font-bold mr-auto">
         Bitrefill Drop
       </Link>
       {/* <NeynarAuthButton
-        className="h-full rounded-full flex items-center "
+        
         label="Sign In"
         variant={SIWN_variant.FARCASTER}
       /> */}
-      <SignInButton />
+      <SignInButton className="h-full rounded-full flex items-center " />
     </header>
   );
 };
