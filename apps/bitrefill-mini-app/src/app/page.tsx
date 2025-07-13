25a4ad691dbcd0881533121e5461d005304c1299
Shadab Khan <shadab.2005@gmail.com>
2025-07-14 03:26:05 +0530

    fix: lint

--- apps/bitrefill-mini-app/src/app/page.tsx
+++ apps/bitrefill-mini-app/src/app/page.tsx
@@ -2,14 +2,8 @@ import HeroSection from './components/HeroSection';
 import ActionButtons from './components/ActionButtons';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
-import { useProfile } from '@farcaster/auth-kit';
 
 export default function Home() {
-  // const {
-  //   isAuthenticated,
-  //   profile: { username, fid },
-  // } = useProfile();
-  // console.log({ username, fid, isAuthenticated });
   return (
     <main className="min-h-screen bg-gray-50 text-gray-800 px-4 py-12">
       <HeroSection />
